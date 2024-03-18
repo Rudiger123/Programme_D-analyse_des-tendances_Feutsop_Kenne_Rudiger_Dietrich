15b52574965c0ab51367c506aa1ed039f043f7a1
Feutsop kenne <feutsoprudiger@gmail.com>
2024-03-18 08:57:47 -0700

    heme Biotech Java

---
+++ Feutsop_Kenne_1_codeComplete_032024/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -0,0 +1,26 @@
+package com.hemebiotech.analytics;
+
+import java.util.HashMap;
+
+public class AnalyticsCounter {
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String args[]) throws Exception {
+
+	//creation d'un objet de la classe ReadSymptomDataFromFile
+	ReadSymptomDataFromFile read = new ReadSymptomDataFromFile("yourFilePath\\Programme d'analyse des tendances_Feutsop_kenne\\Feutsop_Kenne_1_codeComplete_032024\\symptoms.txt");
+		
+	//lecture des symptoms dans le fichier et stockage dans une liste de chaine de caractere ..
+	AnalysticsMethods analystic = new AnalysticsMethods(read.GetSymptoms());
+
+	//la map qui contiendra l'ensemble des symptoms et leurs occurences ..
+	HashMap<String, Integer> counterMap = analystic.occurencesMap();
+
+	//le stockage des symptoms et de leurs occurences dans le fichier result.out....
+	analystic.generateOuput(counterMap);
+
+	}
+}
